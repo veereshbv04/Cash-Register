494663b2d6f8dddaf2ffe8ac55a535103dd27365
Veeresh B V <Veereshbv04@gmail.com>
2021-07-31 23:59:01 +0530

    feat:bases done

--- app.js
+++ app.js
@@ -2,9 +2,30 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-amount");
 
 const checkButton = document.querySelector("#check-button");
+const outDivMessage = document.querySelector("#output-message");
 
 
 checkButton.addEventListener("click",() =>{
-    console.log(billAmount.value);
-    console.log(cashGiven.value);
+    outDivMessage.style.display="none"
+    if(billAmount.value > 0){
+
+        if(cashGiven.value<billAmount.value){
+            outDivMessage.style.display="block";
+            outDivMessage.innerHTML="Cash given should be atlease equal to bill amount";
+        else{
+            const amountToReturn = cashGiven.value - billAmount.value;
+
+            
+        }
+
+    }else{
+        outDivMessage.style.display="block";
+        outDivMessage.innerHTML="bill should be greater than 0";
+        console.log("ello")
+    }
+
 })
+
+function calculateNotesToreturn (returnAmount){
+    
+}

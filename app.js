8417ac6087c677d482e46aba0d74d3a82c00ef3a
Veeresh B V <Veereshbv04@gmail.com>
2021-08-01 17:32:51 +0530

    fix:bug fixed all working properly

--- app.js
+++ app.js
@@ -4,35 +4,45 @@ const cashGiven = document.querySelector("#cash-amount");
 const checkButton = document.querySelector("#check-button");
 const outDivMessage = document.querySelector("#output-message");
 const tableColumn = document.querySelectorAll(".no-of-notes");
+const wholeTable = document.querySelector(".change-table");
 
 const notes = [2000, 500, 100, 20, 10, 5, 1]
 
-checkButton.addEventListener("click",() =>{
-    outDivMessage.style.display="none"
-    if(billAmount.value > 0){
-        console.log("bill is positive");
-        if(cashGiven.value >= billAmount.value){
+checkButton.addEventListener("click",(e) =>{
+    wholeTable.style.display="none";
+    e.preventDefault();
+    clearNotesTable();
+    hideShowerror();
 
-            console.log("calculating notes to return");
-            const amountToReturn = cashGiven.value - billAmount.value;
-            calculateNotesToreturn(amountToReturn);
-        }
-        else{
-            showError("Cash given is less than Bill maount");
-            console.log(billAmount.value);
-            console.log(cashGiven.value);
-            console.log("cash is less than bill");
-            
-        }
+    const bill = Number(billAmount.value);
+    const cash = Number(cashGiven.value);
+    console.log(bill);
+    console.log(cash);
 
+    if(bill>0 && cash>0){
+
+        if(bill > cash){
+            console.log("cash",cash);
+            console.log("bill",bill);
+            showError("Cash is less than bill amount");
+            wholeTable.style.display="none";
+            return;
+        }
+        wholeTable.style.display="block";
+        const amounToReturn = cash - bill;
+        calculateNotesToreturn(amounToReturn);
     }else{
-       showError("Bill should be greater than 0");
-        
+        showError("Enter valid details");
+        wholeTable.style.display="none";
+        return;
     }
+  
 
-})
+    
+});
 
 function calculateNotesToreturn(returnAmount){
+    wholeTable.style.display="block";
     console.log("in calculate function");
     let numberOfNotes = 0;
     for(let i=0;i<=notes.length;i++){
@@ -44,6 +54,17 @@ function calculateNotesToreturn(returnAmount){
     
 }
 
+
+function hideShowerror(){
+    outDivMessage.style.display="none";
+}
+
+function clearNotesTable(){
+    
+   for(let notes in tableColumn){
+       notes.innerHTML = " ";
+   }
+}
 
 function showError(message){
     outDivMessage.style.display = "block";
